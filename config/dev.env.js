296ebabcf34664b9a89cf3fa9dc24c79985e3f89
tiangao-office <tiangao@edenep.net>
2018-01-30 17:54:27 +0800

    clr

--- config/dev.env.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    NODE_ENV: '"development"',
-    ENV_CONFIG: '"dev"',
-    BASE_API: '"http://192.168.0.129:8081"',
-    APP_ORIGIN: '"https://wallstreetcn.com"'
-}
--- start.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-D:
-cd D:\develop\workspace\idea_workspace\base\edenep-manage-plat-web
-echo
-npm run dev
